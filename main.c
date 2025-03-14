15b8e966e5b47551e2637eef2c2520b428fbeda8
Tait-Sherfinski <112713242+Tait-Sherfinski@users.noreply.github.com>
2025-03-14 15:26:40 +0000

    update 3/14

--- main.c
+++ main.c
@@ -21,13 +21,32 @@ void addExpense(char *name, float price) {
     } else printf("List is full");
 }
 
+void removeExpense(char *name) {
+    for (int i = 0; i < itemCount; i++) {
+        if (strcmp(allExpenses[i].name, name) == 0) {
+            for  (int j = i; j < itemCount; j++) {
+                allExpenses[j] = allExpenses[j + 1];
+            }
+            itemCount--;
+            printf("%s has been removed", name);
+            return;
+        }
+    } printf("Item not found");
+}
+
+void showList() {
+    for (int i = 0; i < itemCount; i++) {
+        printf("\n%s %.2f\n", allExpenses[i].name, allExpenses[i].price);
+    }
+}
+
 void calculateNetEarnings(float income) {
     float totalSpending;
     for (int i = 0; i < itemCount; i++) {
         totalSpending += allExpenses[i].price;
     } 
     float net = income - totalSpending;
-    printf("\nNet Earnings: %.2f\n", net);
+    printf("\nNet Earnings: $%.2f\n", net);
 }
 
 int main() {
@@ -40,13 +59,15 @@ int main() {
     scanf("%f", &income);
 
     while (income > 0) {
-        printf("\n1. Add Expense");
-        printf("\n2. Show Net Earnings");
-        printf("\n3. Exit");
+        printf("\n1: Add Expense");
+        printf("\n2: Remove Expense");
+        printf("\n3: Show list of expenses");
+        printf("\n4: Show Net Earnings");
+        printf("\n5: Exit");
         printf("\nEnter choice(number): ");
         scanf("%d", &choice);
 
-        while (choice != 3) {
+        while (choice != 5) {
             if (choice == 1) {
                 printf("\nEnter name of item/expense: ");
                 getchar();
@@ -62,9 +83,25 @@ int main() {
                 break;
             }
             if (choice == 2) {
+                printf("\nName of expense: ");
+                getchar();
+                fgets(name, sizeof(name), stdin);
+                name[strcspn(name, "\n")] = 0;
+                removeExpense(name);
+                break;
+            }
+            if (choice == 3) {
+                showList();
+                break;
+            }
+            if (choice == 4) {
                 calculateNetEarnings(income);
                 break;
             }
+        }
+        if (choice == 5) {
+            printf("Program closed");
+            break;
         }
     } 
 }
